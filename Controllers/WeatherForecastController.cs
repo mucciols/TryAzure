9b0a57a45cfa0757ac8172f8479b14c1f74e15e0
Simone Muccioli <simone.muccioli@consultant.aruba.it>
2024-03-28 07:31:32 +0100

    aggornamento

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -6,6 +6,8 @@ namespace TryAzure.Controllers
     [Route("[controller]")]
     public class WeatherForecastController : ControllerBase
     {
+        int ciao = 10;
+
         private static readonly string[] Summaries = new[]
         {
             "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
